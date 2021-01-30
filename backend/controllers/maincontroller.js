8276982f1520a957c43f3d39caf1135b88850fdc
Sunil Kumar <sunilssaharan@gmail.com>
2021-01-30 18:01:05 +0900

    backend

---
+++ backend/controllers/maincontroller.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+    home:home,
+    login:login,
+    signup:signup
+}
+
+//==========================================================
+//========== get request handling==========================
+//==========================================================
+function home(req,res){
+    res.render('index.ejs',);
+}
+
+function login(argument) {
+    // body...
+}
+
+
+function signup(argument) {
+    // body...
+}
